27305aa5dbcefac6d6f18f7e5a3e56a386d21c78
tokfrans03 <34898241+tokfrans03@users.noreply.github.com>
2020-12-27 22:10:42 +0100

    Initial commit

---
+++ src/main/java/me/tok1/centralchatcontrol/CentralChatControl.java
@@ -0,0 +1,10 @@
+package me.tok1.centralchatcontrol;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CentralChatControl implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
